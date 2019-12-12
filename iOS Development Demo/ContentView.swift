b0986f8253735c86de3f70c3b44008c9eb1c55f3
Brian Alvarez <Balvare@att.net>
2019-12-12 11:59:28 -0600

    Added new Swift Project

---
+++ iOS Development Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS Development Demo
+//
+//  Created by Brian Alvarez on 12/12/19.
+//  Copyright © 2019 Balvee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
